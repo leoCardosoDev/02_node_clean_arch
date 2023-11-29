44395a39ae752c98f2012a740cd73c63ee17675b
leoCardosoDev <leocardosodev@gmail.com>
2023-11-29 18:36:20 -0300

    feat: ensure FAS calls CreateFacebookAccountRepository when LoadFacebookUseApi returns undefined

--- src/application/services/facebook-authentication.ts
+++ src/application/services/facebook-authentication.ts
@@ -1,17 +1,19 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { LoadFacebookUserApi } from '@/application/contracts/apis'
-import { LoadUserAccountRepository } from '@/application/contracts/repository'
+import { CreateFacebookAccountRepository, LoadUserAccountRepository } from '@/application/contracts/repository'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly loadFacebookUserApi: LoadFacebookUserApi,
-    private readonly loadUserAccountRepository: LoadUserAccountRepository) {}
+    private readonly loadUserAccountRepository: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepository: CreateFacebookAccountRepository) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.loadFacebookUserApi.loadUser(params)
     if (fbData !== undefined) {
       await this.loadUserAccountRepository.load({ email: fbData.email })
+      await this.createFacebookAccountRepository.createFromFacebook(fbData)
     }
     return new AuthenticationError()
   }
