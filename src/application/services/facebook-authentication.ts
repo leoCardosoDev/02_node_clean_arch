b9181d03e23e387e629aec7d45175f9bda3d0a24
leoCardosoDev <leocardosodev@gmail.com>
2023-12-01 19:00:38 -0300

    feat: ensure Service calls YokenGenerator with correct params

--- src/application/services/facebook-authentication.ts
+++ src/application/services/facebook-authentication.ts
@@ -2,19 +2,23 @@ import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { FacebookAccount } from '@/domain/models'
 import { LoadFacebookUserApi } from '@/application/contracts/apis'
+import { TokenGenerator } from '@/application/contracts/crypto'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '@/application/contracts/repository'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository) {}
+    private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository,
+    private readonly crypto: TokenGenerator
+  ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.facebookApi.loadUser(params)
     if (fbData !== undefined) {
       const accountData = await this.userAccountRepository.load({ email: fbData.email })
       const fbAccount = new FacebookAccount(fbData, accountData)
-      await this.userAccountRepository.saveWithFacebook(fbAccount)
+      const { id } = await this.userAccountRepository.saveWithFacebook(fbAccount)
+      await this.crypto.generateToken({ key: id })
     }
     return new AuthenticationError()
   }
