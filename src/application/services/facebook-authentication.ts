99666183016394000d1eb3f7812a29c084161b10
leoCardosoDev <leocardosodev@gmail.com>
2023-12-05 14:48:53 -0300

    feat: ensure Service returns an AccessToken on success

--- src/application/services/facebook-authentication.ts
+++ src/application/services/facebook-authentication.ts
@@ -1,24 +1,26 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken, FacebookAccount } from '@/domain/models'
+
 import { LoadFacebookUserApi } from '@/application/contracts/apis'
 import { TokenGenerator } from '@/application/contracts/crypto'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '@/application/contracts/repository'
 
-export class FacebookAuthenticationService {
+export class FacebookAuthenticationService implements FacebookAuthentication {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
     private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository,
     private readonly crypto: TokenGenerator
   ) {}
 
-  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
+  async perform (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
     const fbData = await this.facebookApi.loadUser(params)
     if (fbData !== undefined) {
       const accountData = await this.userAccountRepository.load({ email: fbData.email })
       const fbAccount = new FacebookAccount(fbData, accountData)
       const { id } = await this.userAccountRepository.saveWithFacebook(fbAccount)
-      await this.crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
+      const token = await this.crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
+      return new AccessToken(token)
     }
     return new AuthenticationError()
   }
