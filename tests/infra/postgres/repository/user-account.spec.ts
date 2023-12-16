31b5a6d6b8df011ee7682650d88fa3a1cbeeb458
leoCardosoDev <leocardosodev@gmail.com>
2023-12-16 08:25:29 -0300

    feat: ensure PgUserAccountRepository returns undefined if email does not exists

--- tests/infra/postgres/repository/user-account.spec.ts
+++ tests/infra/postgres/repository/user-account.spec.ts
@@ -57,6 +57,32 @@ describe('PgUserAccountRepository', () => {
       const sut = new PgUserAccountRepository()
       const account = await sut.load({ email: 'existing_email@mail.com' })
       expect(account).toEqual({ id: '1' })
+      connection.close()
+    })
+
+    it('should returns undefined if email not exists', async () => {
+      const db = newDb()
+
+      db.public.registerFunction({
+        implementation: () => 'test',
+        name: 'current_database'
+      })
+
+      db.public.registerFunction({
+        implementation: () => 'test',
+        name: 'version'
+      })
+
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+
+      await connection.synchronize()
+      const sut = new PgUserAccountRepository()
+      const account = await sut.load({ email: 'new_email@mail.com' })
+      expect(account).toBeUndefined()
+      connection.close()
     })
   })
 })
